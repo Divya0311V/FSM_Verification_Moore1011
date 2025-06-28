e559ec50adff6f9259710465e8b83aa10bfdd2b6
Divya0311V <158789267+Divya0311V@users.noreply.github.com>
2025-06-28 18:27:39 +0530

    FSM_package.svh

---
+++ FSM_package.svh
@@ -0,0 +1,9 @@
+package fsm_sgls;
+ `include "FSM_clk.svh"
+ `include "FSM_rst.svh"
+ `include "FSM_1.svh"
+ `include "FSM_2.svh"
+ `include "FSM_3.svh"
+ `include "FSM_simulus.svh"
+ `include "FSM_runfile.svh"
+endpackage
